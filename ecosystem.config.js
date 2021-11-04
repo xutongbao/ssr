b877e6656c3b9cfccb92727ebecf3213f096f31e
xutongbao <1183391880@qq.com>
2021-11-04 11:58:13 +0800

    优化

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       exec_mode: 'cluster',
       instances: 2, //工作线程数量,如果给定的数字为0，PM2则会根据你CPU核心的数量来生成对应的工作线程。
       script: './node_modules/next/dist/bin/next',
-      args: 'start -p 84'
+      args: `start -p ${process.env.PORT}`
     }
   ]
 }
